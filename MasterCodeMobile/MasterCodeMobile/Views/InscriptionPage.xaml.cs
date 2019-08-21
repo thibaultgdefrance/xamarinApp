bde98ae3672fab822f53bec6497f034713a03a7e
afpa <afpa@STA6018699.exchange.ad.afpanet>
2019-08-21 08:21:02 +0200

    debut

---
+++ MasterCodeMobile/MasterCodeMobile/Views/InscriptionPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterCodeMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InscriptionPage : ContentPage
+    {
+        public InscriptionPage()
+        {
+            InitializeComponent();
+        }
+        public void ajouterkakin(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MasterCodeMobile/MasterCodeMobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterCodeMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+       
+        public LoginPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+
+        public void Redirect_Clicked(object sender, EventArgs e)
+        {
+             Navigation.PushAsync(new InscriptionPage());
+        }
+
+
+
+    }
+}
